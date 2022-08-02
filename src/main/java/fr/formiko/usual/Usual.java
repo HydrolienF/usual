cdd7ecce64fd625f03608cbcb7c48bd3b5d00011
HydrolienF <hydrolien.f@gmail.com>
2022-08-02 13:18:57 +0200

    stats command is now in usual/

--- src/main/java/fr/formiko/usual/Usual.java
+++ src/main/java/fr/formiko/usual/Usual.java
@@ -3,6 +3,7 @@ package fr.formiko.usual;
 import fr.formiko.usual.structures.listes.GString;
 import fr.formiko.usual.images.Images;
 import fr.formiko.usual.types.str;
+import fr.formiko.usual.stats;
 
 /**
 *{@summary Main class with all function that can be launch by args.}<br>
@@ -52,6 +53,69 @@ public class Usual {
         Images.resizeAll(fileName, pixelMax, true);
         break;
       }
+      case "stats":{
+        stats(args);
+        break;
+      }
+      case "statsG":{
+        stats.setOnlyLastLine(true);
+        stats.setSpliter(';');
+        stats2(args);
+        break;
+      }
     }
+  }
+
+
+  /**
+  *{@summary update stats in stats.txt.}<br>
+  *It can also print a value if it have a 2a args that is an integer.
+  *It always print a global value for every file:
+  *<ul>
+  *<li>1: number of lines.
+  *<li>2: number of classes.
+  *<li>3: number of long functions.
+  *<li>4: number of short functions.
+  *</ul>
+  *@lastEditedVersion 2.20
+  */
+  private static void stats(String args[]){
+    int valueToPrint = 0;
+    if(args.length>2){
+      valueToPrint=str.sToI(args[2]);
+    }
+    if(args.length>1){
+      stats.statsJavadoc(args[1], false);
+    }else{
+      stats.statsJavadoc("src/main/java/", true);
+    }
+    if(valueToPrint>0){
+      String s = switch (valueToPrint) {
+        case 1:
+        yield stats.sommeNbrDeLigneG+"";
+        case 2:
+        yield stats.sommeDesClassG+"";
+        case 3:
+        yield stats.sommeDesFctLG+"";
+        case 4:
+        yield stats.sommeDesFctCG+"";
+        default:
+        yield "";
+      };
+      erreur.println(s);
+    }
+  }
+  /**
+  *{@summary Print global stats about the current state of the projet.}<br>
+  *@lastEditedVersion 2.20
+  */
+  private static void stats2(String args[]){
+    String result="";
+    if(args.length>1){
+      result+=stats.getStats(args[1], false).toString();
+    }else{
+      result+=stats.getStats("src/main/java/", true).toString();
+    }
+    erreur.println(result);
   }
 }
